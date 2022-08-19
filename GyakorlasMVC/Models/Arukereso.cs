30c899fd1ddbf56f72738bd7f863c53fe7fd3072
Citrom2021 <citronejoe0123@citromail.hu>
2022-08-19 09:25:19 +0200

    added files for all mvc and crud

---
+++ GyakorlasMVC/Models/Arukereso.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace GyakorlasMVC.Models
+{
+    public class Arukereso
+    {
+        public string megnevezesKereses { get; set; }
+        public string tipusKereses { get; set; }
+        public SelectList tipusLista { get; set; }
+        public List<Adatmodel> Aru { get; set; }
+    }
+}
